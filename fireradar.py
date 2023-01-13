5255aaed87a370d43cfa5c2b419978eabe158d7e
Ethan Jewell <ethanjevvell@gmail.com>
2023-01-13 07:24:43 +0000

    Update script with generic gmail account for alerts

--- fireradar.py
+++ fireradar.py
@@ -10,8 +10,8 @@ from datetime import datetime as dt
 
 def sendAlert(content):  # Sends an email with given content
 
-    user = yagmail.SMTP(user='nknews.thermalradar@gmail.com',
-                        password='pknzhwayprlenjyi')
+    user = yagmail.SMTP(user='nknews.scriptalerts@gmail.com',
+                        password='zjfelavkafmzykiu') # This is the App Password for Python scripts that use this gmail account! Keep it secure.
     user.send(to='ethan.jewell@nknews.org',
                  subject='[FIRE ALERT] -- Fires detected',
                  contents=content)
@@ -52,46 +52,3 @@ if fires_for_email['Province'].str.contains('Pyongyang').any():
 if len(fires_for_email) >= 5:
     sendAlert(
         f'Five or more fires detected in the DPRK. Here\'re the most recent fires: \n {fires_for_email.to_html()}')
-
-
-# %%
-# FOR THE FUTURE -- An attempt at using the API instead of downloading shapefiles with wget
-
-# def createDF():
-
-#     API = 'https://firms.modaps.eosdis.nasa.gov/api/country/csv/a1965c3e5f85c73b483a1ca7d4a0ea36/VIIRS_SNPP_NRT/PRK/3'
-#     response = requests.get(API)
-#     response.text
-
-#     rows = response.text.split('\n')
-#     detailed_rows = []
-
-#     i = 1
-
-#     while i < len(rows):
-#         detailed_rows.append(rows[i].split(','))
-#         i += 1
-
-#     df = pd.DataFrame(detailed_rows, columns=rows[0].split(','))
-#     return df
-# point_df = df[['latitude', 'longitude']]
-# point_tuples = list(point_df.itertuples(index=False, name=None))
-
-# point_poly = []
-
-# for point in point_tuples:
-#     point_poly.append(Point(float(point[0]), float(point[1])))
-
-# xs = [point.x for point in point_poly]
-# ys = [point.y for point in point_poly]
-
-# border = gpd.read_file('/home/ethanjewell/Desktop/Python Env/Scripting/Map Data/Shapefiles/NATIONAL/National Borders with Provinces/National Borders with Provinces.shp')
-# print(point_poly)
-
-# fig, ax = plt.subplots()
-# ax.plot(border)
-
-# %%
-
-
-# %%
